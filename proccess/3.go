3cdcb4498947dfc1d437abd88c901822c22bd564
bloominstrong <aoc@mail.bloominstrong.net>
2023-12-04 07:13:21 +1000

    Day 3 Part 1 WIP Refactored code Currently missing  "123*234" case

--- proccess/3.go
+++ proccess/3.go
@@ -24,6 +24,10 @@ func main() {
 		sum = sum + mySum(prevLine,currentLine,nextLine,1)
 		//fmt.Println(sum)
 	}
+	prevLine=currentLine
+	currentLine=nextLine
+	nextLine=""
+	sum = sum + mySum(prevLine,currentLine,nextLine,1)
 	file.Close()
 	fmt.Println("Version 1 is:",sum)
 
@@ -31,11 +35,14 @@ func main() {
 	file, _ = os.Open("../input/3")
 	scanner2 := bufio.NewScanner(file)
 	sum = 0
+	prevLine = ""
+	currentLine = ""
+	nextLine = string(scanner2.Bytes())
 	for scanner2.Scan() {	
 		prevLine = currentLine
 		currentLine = nextLine
-		nextLine = string(scanner.Bytes())
-		sum = sum + mySum(prevLine,currentLine,nextLine,2)
+		nextLine = string(scanner2.Bytes())
+		//sum = sum + mySum(prevLine,currentLine,nextLine,2)
 		//fmt.Println(sum)
 	}
 	fmt.Println("Version 2 is:",sum)
@@ -47,35 +54,40 @@ func mySum (prevLine string, currentLine string, nextLine string ,version int) i
 	currentItems := strings.Split(currentLine,".")
 	//nextItems := strings.Split(nextLine,".")
 	lineSum := 0
-
+	charList := "!@#$%^&*()-_=+ /,;:'[]{}|<>?"
 	for _, data := range currentItems {
 		if (strings.ContainsAny(data,"0123456789")) {
 			//data is a number
 			startPos := strings.Index(currentLine,data)-1
-			if (unicode.IsSymbol(rune(data[0]))) || (startPos == -1){
+			if (unicode.IsPunct(rune(data[0]))) || (startPos == -1){
 				startPos++
 			}
 			endPos := startPos+len(data)+1
-			if (unicode.IsSymbol(rune(data[len(data)-1]))) || (endPos == len(currentLine)) {
+			if (unicode.IsPunct(rune(data[len(data)-1]))) || (endPos == len(currentLine)) {
 				endPos--
 			}
-			fmt.Println("Data:",data,"CurLine:",currentLine,"LineLen:",len(prevLine),"Start:",startPos,"End:",endPos)
-			for i := startPos; i < endPos; i++ {
+			fmt.Println("Data:",data,"\nPL:",prevLine,"\nCL:",currentLine,"\nNL:",nextLine,"Start:",startPos,"End:",endPos)
+			for i := startPos; i <= endPos; i++ {
 				if (endPos < len(prevLine)) {
-					if (unicode.IsSymbol(rune(prevLine[i]))) {
-						dataAsNum, _ := strconv.Atoi(data) 
+					//fmt.Println("Pi:",i,prevLine[i])
+					if (strings.ContainsAny(string(prevLine[i]),charList)) {
+						dataAsNum, _ := strconv.Atoi(data)
+						fmt.Println("Found Prev:",data)
 						lineSum = lineSum + dataAsNum
 					}
 				}
-				if (endPos < len(nextLine)) {
-					if (unicode.IsSymbol(rune(nextLine[i]))) && (endPos<len(nextLine))  {
+				if (endPos < len(nextLine)) {	
+					if (strings.ContainsAny(string(nextLine[i]),charList)) {
 						dataAsNum, _ := strconv.Atoi(data) 
+						fmt.Println("Found Next:",data)
 						lineSum = lineSum + dataAsNum
 					}
 				}
 			}
-			if (unicode.IsSymbol(rune(currentLine[startPos]))) || (unicode.IsSymbol(rune(currentLine[endPos]))) {
-				dataAsNum, _ := strconv.Atoi(data) 
+			if (strings.ContainsAny(data,charList)) {
+				dataAsNum, _ := strconv.Atoi(strings.Trim(data,charList))
+			
+				fmt.Println("Found Cur:",dataAsNum)
 				lineSum = lineSum + dataAsNum
 			}
 		}
