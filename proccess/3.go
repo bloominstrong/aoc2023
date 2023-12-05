612cdad89e709c00b12797e8f5933a28ba67520f
bloominstrong <aoc@mail.bloominstrong.net>
2023-12-05 22:16:27 +1000

    Day 3 Part 1 still WIP Stripped symbols from numbers on current lines Lots more debugging added

--- proccess/3.go
+++ proccess/3.go
@@ -51,46 +51,55 @@ func main() {
 func mySum (prevLine string, currentLine string, nextLine string ,version int) int {
 
 	//prevItems := strings.Split(prevLine,".")
-	currentItems := strings.Split(currentLine,".")
+	//currentItems := strings.Split(currentLine,".")
 	//nextItems := strings.Split(nextLine,".")
 	lineSum := 0
-	charList := "!@#$%^&*()-_=+ /,;:'[]{}|<>?"
-	for _, data := range currentItems {
-		if (strings.ContainsAny(data,"0123456789")) {
-			//data is a number
+	charList := "!@#$%^&*()-_=+ /,;:'[]{}|<>?`~"
+	currentNumbers := strings.FieldsFunc(currentLine, func(c rune) bool { return !unicode.IsNumber(c)})
+	found := false
+	for _, data := range currentNumbers {
+			found = false
 			startPos := strings.Index(currentLine,data)-1
-			if (unicode.IsPunct(rune(data[0]))) || (startPos == -1){
-				startPos++
-			}
 			endPos := startPos+len(data)+1
-			if (unicode.IsPunct(rune(data[len(data)-1]))) || (endPos == len(currentLine)) {
+			if (endPos == len(currentLine)) {
 				endPos--
 			}
-			fmt.Println("Data:",data,"\nPL:",prevLine,"\nCL:",currentLine,"\nNL:",nextLine,"Start:",startPos,"End:",endPos)
+			if (startPos == -1){
+				startPos++
+				
+			}
+			//fmt.Println("Data:",data,"\nPL:",prevLine,"\nCL:",currentLine,"\nNL:",nextLine,"Start:",startPos,"End:",endPos)
+		PosLoop:
 			for i := startPos; i <= endPos; i++ {
 				if (endPos < len(prevLine)) {
-					//fmt.Println("Pi:",i,prevLine[i])
 					if (strings.ContainsAny(string(prevLine[i]),charList)) {
 						dataAsNum, _ := strconv.Atoi(data)
-						fmt.Println("Found Prev:",data)
+						//fmt.Println("Found Prev:",data)
 						lineSum = lineSum + dataAsNum
+						found = true
+						break PosLoop
 					}
 				}
 				if (endPos < len(nextLine)) {	
 					if (strings.ContainsAny(string(nextLine[i]),charList)) {
 						dataAsNum, _ := strconv.Atoi(data) 
-						fmt.Println("Found Next:",data)
+						//fmt.Println("Found Next:",data)
 						lineSum = lineSum + dataAsNum
+						found = true
+						break PosLoop
 					}
 				}
+				if (strings.ContainsAny(string(currentLine[i]),charList)) {
+					dataAsNum, _ := strconv.Atoi(data)
+					//fmt.Println("Found Cur:",dataAsNum)
+					lineSum = lineSum + dataAsNum
+					found = true
+					break PosLoop
+				}
 			}
-			if (strings.ContainsAny(data,charList)) {
-				dataAsNum, _ := strconv.Atoi(strings.Trim(data,charList))
-			
-				fmt.Println("Found Cur:",dataAsNum)
-				lineSum = lineSum + dataAsNum
+			if (!found){
+				fmt.Println("Data:",data,"\nPL:",prevLine,"\nCL:",currentLine,"\nNL:",nextLine,"Start:",startPos,"End:",endPos)
 			}
-		}
 	}
 
 	if (version == 2){
